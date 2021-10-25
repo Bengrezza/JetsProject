d8d6d0350dbc7d3e6f4918d8a70e494ea87d82ff
Bengrezza <bengretza@gmail.com>
2021-10-25 07:46:17 -0600

    initial commit

---
+++ src/com/skilldistillery/jets/app/JetsApplication.java
@@ -0,0 +1,11 @@
+package com.skilldistillery.jets.app;
+
+import com.skilldistillery.jets.entities.AirField;
+
+public class JetsApplication {
+		private AirField airField;
+	
+		public static void main(String[] args) {
+			
+		}
+}
